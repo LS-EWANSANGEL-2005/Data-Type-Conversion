f386f6c55b1b6e172e39174319274c64cc977c2f
Ewans Angel LS <ewansangel2005@gmail.com>
2026-08-18 12:16:49 +0530

    Add files via upload

---
+++ IntTOStr.java
@@ -0,0 +1,13 @@
+package datatypeconversion;
+
+public class IntTOStr {
+
+	public static void main(String[] args) {
+		int num=10;
+		String str=""+num;
+		System.out.println("Integer:"+num);
+		System.out.println("String:"+str);
+		System.out.println("concatenated String:"+(str+1));
+	}
+
+}
